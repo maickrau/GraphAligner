c533eba91f73a5f6ea12906bbe35374bfd0b020a
Mikko Rautiainen <m_rautiainen@hotmail.com>
2017-05-31 15:24:17 +0200

    Move the aligner to its own file

---
+++ Aligner.h
@@ -0,0 +1,10 @@
+#ifndef Aligner_h
+#define Aligner_h
+
+#include <string>
+#include <vector>
+#include "vg.pb.h"
+
+void alignReads(std::string graphFile, std::string fastqFile, std::string seedFile, int numThreads, int bandwidth, std::string alignmentFile);
+
+#endif
