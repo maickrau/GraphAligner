8981de0c06e0614b6749c3ddc06de5f210579e40
Mikko Rautiainen <m_rautiainen@hotmail.com>
2017-05-31 15:24:52 +0200

    forgot this earlier

---
+++ ReverseReads.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include "fastqloader.h"
+
+int main(int argc, char** argv)
+{
+	auto reads = loadFastqFromFile(argv[1]);
+	std::ofstream output {argv[2]};
+	for (size_t i = 0; i < reads.size(); i++)
+	{
+		auto reverse = reads[i].reverseComplement();
+		output << "@" << reverse.seq_id << "\n";
+		output << reverse.sequence << "\n";
+		output << "+\n";
+		output << reverse.quality << "\n";
+	}
+}
