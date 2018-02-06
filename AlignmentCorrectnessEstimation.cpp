205c4dc6ccbf43972eb674559ee7a4ddb9847b77
Mikko Rautiainen <m_rautiainen@hotmail.com>
2018-02-06 14:07:29 +0100

    Estimate the errors per slice as normally distributed, use empirical parameters

--- AlignmentCorrectnessEstimation.cpp
+++ AlignmentCorrectnessEstimation.cpp
@@ -3,27 +3,71 @@
 #include "AlignmentCorrectnessEstimation.h"
 #include "ThreadReadAssertion.h"
 
-const double correctMismatchLogProbability = log(0.2); //20% from observed ONT error rate when aligning ONT's to human DBG
-const double correctMatchLogProbability = log(1.0 - 0.2);
-const double falseMismatchLogProbability = log(0.5); //50% empirically
-const double falseMatchLogProbability = log(1.0 - 0.5);
-const double falseToCorrectTransitionLogProbability = log(0.00001); //10^-5. one slice at <=12 mismatches or two slices at <=15 mismatchs
-const double falseToFalseTransitionLogProbability = log(1.0 - 0.00001);
-const double correctToFalseTransitionLogProbability = log(0.000000000000001); //10^-15. three slices at >=25 mismatches or two slices at >=30 mismatches
-const double correctToCorrectTransitionLogProbability = log(1.0 - 0.000000000000001);
+//empirically from aligning one ONT to its correct position in the genome
+const double correctMean = 0.1875;
+const double correctStddev = 0.0955;
+//empirically from aligning one random read to a position
+const double wrongMean = 0.5;
+const double wrondStddev = 0.0291;
 
-std::vector<double> getLogFactorials()
+const int wordSize = 64;
+
+const double falseToCorrectTransitionLogProbability = log(0.00001); //10^-5. arbitrary.
+const double falseToFalseTransitionLogProbability = log(1.0 - 0.00001);
+const double correctToFalseTransitionLogProbability = log(0.0000000001); //10^-10. arbitrary.
+const double correctToCorrectTransitionLogProbability = log(1.0 - 0.0000000001);
+
+double stddistlog(double val, double mean, double stddev)
+{
+	return -(val-mean)*(val-mean)/(2*stddev*stddev);
+}
+
+void normalize(std::vector<double>& logs)
+{
+	double sum = 0;
+	for (auto x : logs)
+	{
+		sum += exp(x);
+	}
+	double add = log(1.0/sum);
+	for (auto& x : logs)
+	{
+		x += add;
+	}
+}
+
+std::vector<double> getCorrectLogOdds()
 {
 	std::vector<double> result;
-	result.push_back(0);
-	for (int i = 1; i <= 64; i++)
+	for (int i = 0; i < wordSize/2; i++)
 	{
-		result.push_back(result.back() + log(i));
+		result.push_back(stddistlog(i, correctMean*wordSize, correctStddev*wordSize));
+	}
+	normalize(result);
+	for (int i = wordSize/2; i < wordSize; i++)
+	{
+		result.push_back(result.back());
 	}
 	return result;
 }
 
-const std::vector<double> logFactorials = getLogFactorials();
+std::vector<double> getWrongLogOdds()
+{
+	std::vector<double> result;
+	for (int i = 0; i <= wordSize/2; i++)
+	{
+		result.push_back(stddistlog(i, wrongMean*wordSize, wrondStddev*wordSize));
+	}
+	normalize(result);
+	for (int i = wordSize/2; i < wordSize; i++)
+	{
+		result.push_back(result.back());
+	}
+	return result;
+}
+
+const std::vector<double> precomputedCorrectLogOdds = getCorrectLogOdds();
+const std::vector<double> precomputedWrongLogOdds = getWrongLogOdds();
 
 AlignmentCorrectnessEstimationState::AlignmentCorrectnessEstimationState() :
 correctLogOdds(log(0.8)), //80% arbitrarily
@@ -48,16 +92,6 @@ bool AlignmentCorrectnessEstimationState::FalseFromCorrect() const
 	return falseFromCorrectTrace;
 }
 
-double logChoose(int n, int k)
-{
-	return logFactorials[n] - logFactorials[k] - logFactorials[n - k];
-}
-
-double logPowr(double logBase, int exponent)
-{
-	return exponent * logBase;
-}
-
 double AlignmentCorrectnessEstimationState::CorrectLogOdds() const
 {
 	return correctLogOdds;
@@ -70,7 +104,8 @@ double AlignmentCorrectnessEstimationState::FalseLogOdds() const
 
 AlignmentCorrectnessEstimationState AlignmentCorrectnessEstimationState::NextState(int mismatches, int rowSize) const
 {
-	assert(rowSize == 64 || rowSize == 1);
+	assert(rowSize == 64);
+	// assert(rowSize == 64 || rowSize == 1);
 	assert(mismatches >= 0);
 	assert(mismatches <= rowSize);
 	AlignmentCorrectnessEstimationState result;
@@ -78,11 +113,8 @@ AlignmentCorrectnessEstimationState AlignmentCorrectnessEstimationState::NextSta
 	result.falseFromCorrectTrace = correctLogOdds + correctToFalseTransitionLogProbability >= falseLogOdds + falseToFalseTransitionLogProbability;
 	double newCorrectProbability = std::max(correctLogOdds + correctToCorrectTransitionLogProbability, falseLogOdds + falseToCorrectTransitionLogProbability);
 	double newFalseProbability = std::max(correctLogOdds + correctToFalseTransitionLogProbability, falseLogOdds + falseToFalseTransitionLogProbability);
-	auto chooseresult = logChoose(rowSize, mismatches);
-	auto correctMultiplier = chooseresult + logPowr(correctMismatchLogProbability, mismatches) + logPowr(correctMatchLogProbability, rowSize - mismatches);
-	auto falseMultiplier = chooseresult + logPowr(falseMismatchLogProbability, mismatches) + logPowr(falseMatchLogProbability, rowSize - mismatches);
-	newCorrectProbability += correctMultiplier;
-	newFalseProbability += falseMultiplier;
+	newCorrectProbability += precomputedCorrectLogOdds[mismatches];
+	newFalseProbability += precomputedWrongLogOdds[mismatches];
 	result.correctLogOdds = newCorrectProbability;
 	result.falseLogOdds = newFalseProbability;
 	return result;
