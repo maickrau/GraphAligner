758f96b96f2ad0e0bff8f8f97d7b9a85870f96ac
Mikko Rautiainen <m_rautiainen@hotmail.com>
2018-04-16 14:45:24 +0200

    PickSeedHits works for mummer seeds too

--- PickSeedHits.cpp
+++ PickSeedHits.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 #include <string>
 #include <algorithm>
 #include "vg.pb.h"
 #include "stream.hpp"
+#include "CommonUtils.h"
 
 bool seedHitExists(const vg::Alignment& seedHit, const std::vector<vg::Alignment>& existing)
 {
@@ -13,25 +14,37 @@ bool seedHitExists(const vg::Alignment& seedHit, const std::vector<vg::Alignment
 	});
 }
 
+int seedLength(const vg::Alignment& seedHit)
+{
+	if (seedHit.path().mapping_size() == 0) return 0;
+	if (seedHit.path().mapping(0).edit_size() == 0) return 0;
+	return seedHit.path().mapping(0).edit(0).from_length();
+}
+
 int main(int argc, char** argv)
 {
-	std::map<std::string, std::vector<vg::Alignment>> alignments;
-	int maxseeds = std::stoi(argv[2]);
+	std::string outputFileName { argv[1] };
+	int maxSeeds = std::stoi(argv[2]);
+	std::unordered_map<std::string, std::vector<vg::Alignment>> alignments;
 	for (int i = 3; i < argc; i++)
 	{
-		std::ifstream seedfile { argv[i], std::ios::in | std::ios::binary };
-		std::function<void(vg::Alignment&)> alignmentLambda = [&alignments, maxseeds](vg::Alignment& a) {
-			if (a.path().mapping(0).position().node_id() <= 1) return;
-			if (seedHitExists(a, alignments[a.name()])) return;
-			if (alignments[a.name()].size() < maxseeds) alignments[a.name()].push_back(a);
-		};
-		stream::for_each(seedfile, alignmentLambda);
+		auto fileAlns = CommonUtils::LoadVGAlignments(argv[i]);
+		for (auto a : fileAlns)
+		{
+			alignments[a.name()].push_back(a);
+		}
 	}
 	std::vector<vg::Alignment> writeAlignments;
-	for (auto pair : alignments)
+	for (auto& pair : alignments)
 	{
-		writeAlignments.insert(writeAlignments.end(), pair.second.begin(), pair.second.end());
+		std::stable_sort(pair.second.begin(), pair.second.end(), [](const vg::Alignment& left, const vg::Alignment& right) { return seedLength(left) > seedLength(right); });
+		std::vector<vg::Alignment> result;
+		for (int i = 0; result.size() < maxSeeds && i < pair.second.size(); i++)
+		{
+			if (!seedHitExists(pair.second[i], result)) result.push_back(pair.second[i]);
+		}
+		writeAlignments.insert(writeAlignments.end(), result.begin(), result.end());
 	}
-	std::ofstream alignmentOut { argv[1], std::ios::out | std::ios::binary };
+	std::ofstream alignmentOut { outputFileName, std::ios::out | std::ios::binary };
 	stream::write_buffered(alignmentOut, writeAlignments, 0);
 }
