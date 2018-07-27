fadc6dbbd2f57b7a88500d695db8dfb1f68fd3ac
Mikko Rautiainen <m_rautiainen@hotmail.com>
2018-07-27 13:40:57 +0200

    AddNodeNames streams instead of reading all at once

--- AddNodeNames.cpp
+++ AddNodeNames.cpp
@@ -21,18 +21,25 @@ int main(int argc, char** argv)
 		}
 	}
 
-	auto alns = CommonUtils::LoadVGAlignments(inputAlignmentFile);
-	for (auto& aln : alns)
-	{
-		for (int i = 0; i < aln.path().mapping_size(); i++)
+
+
+	std::vector<vg::Alignment> writeAlns;
+	std::ifstream alnfile { inputAlignmentFile, std::ios::in | std::ios::binary };
+	std::ofstream outfile { outputAlignmentFile,  std::ios::out | std::ios::binary };
+	std::function<void(vg::Alignment&)> lambda = [&writeAlns, &outfile, &mapping](vg::Alignment& g) {
+		for (int i = 0; i < g.path().mapping_size(); i++)
 		{
-			auto position = aln.mutable_path()->mutable_mapping(i)->mutable_position();
+			auto position = g.mutable_path()->mutable_mapping(i)->mutable_position();
 			assert(position->node_id() > 0);
 			assert(position->node_id() < mapping.size());
 			position->set_name(mapping[position->node_id()]);
 		}
-	}
-
-	std::ofstream outfile { outputAlignmentFile,  std::ios::out | std::ios::binary };
-	stream::write_buffered(outfile, alns, 0);
+		writeAlns.push_back(g);
+		if (writeAlns.size() > 10000)
+		{
+			stream::write_buffered(outfile, writeAlns, 0);
+			writeAlns.clear();
+		}
+	};
+	stream::for_each(alnfile, lambda);
 }
