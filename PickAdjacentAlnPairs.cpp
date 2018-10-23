c20fb6b74852160aa6a39bd1dfa048c678988535
Mikko Rautiainen <m_rautiainen@hotmail.com>
2018-10-23 14:17:23 +0200

    pick partial alignments crossing a gap

---
+++ PickAdjacentAlnPairs.cpp
@@ -0,0 +1,73 @@
+#include <unordered_map>
+#include <fstream>
+#include "vg.pb.h"
+#include "stream.hpp"
+#include "CommonUtils.h"
+#include "fastqloader.h"
+
+std::vector<vg::Alignment> pickPairs(const std::vector<vg::Alignment>& alns, const std::unordered_map<std::string, size_t>& readLens, int maxSplitDist)
+{
+	std::unordered_map<std::string, std::vector<const vg::Alignment*>> startsPerRead;
+	std::unordered_map<std::string, std::vector<const vg::Alignment*>> endsPerRead;
+	for (auto& aln : alns)
+	{
+		assert(readLens.count(aln.name()) == 1);
+		if (aln.query_position() == 0)
+		{
+			startsPerRead[aln.name()].push_back(&aln);
+		}
+		if (aln.query_position() + aln.sequence().size() == readLens.at(aln.name()))
+		{
+			endsPerRead[aln.name()].push_back(&aln);
+		}
+	}
+	std::vector<vg::Alignment> result;
+	for (auto pair : startsPerRead)
+	{
+		size_t currentPairNum = 0;
+		for (auto start : pair.second)
+		{
+			for (auto end : endsPerRead[pair.first])
+			{
+				assert(start->query_position() == 0);
+				int startEnd = start->sequence().size();
+				int endStart = end->query_position();
+				if (abs(startEnd-endStart) > maxSplitDist) continue;
+				vg::Alignment left { *start };
+				vg::Alignment right { *end };
+				left.set_name(pair.first + "_pair" + std::to_string(currentPairNum) + "_1");
+				right.set_name(pair.first + "_pair" + std::to_string(currentPairNum) + "_2");
+				result.push_back(std::move(left));
+				result.push_back(std::move(right));
+				currentPairNum++;
+			}
+		}
+	}
+	return result;
+}
+
+std::unordered_map<std::string, size_t> getReadLens(std::string filename)
+{
+	auto reads = loadFastqFromFile(filename);
+	std::unordered_map<std::string, size_t> result;
+	for (auto read : reads)
+	{
+		result[read.seq_id] = read.sequence.size();
+	}
+	return result;
+}
+
+int main(int argc, char** argv)
+{
+	std::string inputAlns { argv[1] };
+	int maxSplitDist = std::stoi(argv[2]);
+	std::string readFile { argv[3] };
+	std::string outputAlns { argv[4] };
+
+	auto readLens = getReadLens(readFile);
+	auto alns = CommonUtils::LoadVGAlignments(inputAlns);
+	auto pairs = pickPairs(alns, readLens, maxSplitDist);
+
+	std::ofstream alignmentOut { outputAlns, std::ios::out | std::ios::binary };
+	stream::write_buffered(alignmentOut, pairs, 0);
+}
--- makefile
+++ makefile
@@ -81,7 +81,10 @@ $(BINDIR)/BruteForceExactPrefixSeeds: BruteForceExactPrefixSeeds.cpp $(ODIR)/Com
 $(BINDIR)/UntipRelative: UntipRelative.cpp $(ODIR)/CommonUtils.o $(ODIR)/vg.pb.o $(ODIR)/GfaGraph.o $(ODIR)/fastqloader.o $(ODIR)/ThreadReadAssertion.o
 	$(GPP) -o $@ $^ $(LINKFLAGS)
 
-all: $(BINDIR)/Aligner $(BINDIR)/SimulateReads $(BINDIR)/ReverseReads $(BINDIR)/SupportedSubgraph $(BINDIR)/MafToAlignment $(BINDIR)/ExtractPathSequence $(BINDIR)/ExtractPathSubgraphNeighbourhood $(BINDIR)/VisualizeAlignment $(BINDIR)/NodePosCsv $(BINDIR)/ExtractExactPathSubgraph $(BINDIR)/EstimateRepeatCount $(BINDIR)/PickMummerSeeds $(BINDIR)/SelectLongestAlignment $(BINDIR)/Postprocess $(BINDIR)/AlignmentSubsequenceIdentity $(BINDIR)/BruteForceExactPrefixSeeds
+$(BINDIR)/PickAdjacentAlnPairs: PickAdjacentAlnPairs.cpp $(ODIR)/CommonUtils.o $(ODIR)/vg.pb.o $(ODIR)/GfaGraph.o $(ODIR)/fastqloader.o $(ODIR)/ThreadReadAssertion.o
+	$(GPP) -o $@ $^ $(LINKFLAGS)
+
+all: $(BINDIR)/Aligner $(BINDIR)/SimulateReads $(BINDIR)/ReverseReads $(BINDIR)/SupportedSubgraph $(BINDIR)/MafToAlignment $(BINDIR)/ExtractPathSequence $(BINDIR)/ExtractPathSubgraphNeighbourhood $(BINDIR)/VisualizeAlignment $(BINDIR)/NodePosCsv $(BINDIR)/ExtractExactPathSubgraph $(BINDIR)/EstimateRepeatCount $(BINDIR)/PickMummerSeeds $(BINDIR)/SelectLongestAlignment $(BINDIR)/Postprocess $(BINDIR)/AlignmentSubsequenceIdentity $(BINDIR)/BruteForceExactPrefixSeeds $(BINDIR)/PickAdjacentAlnPairs
 
 clean:
 	rm -f $(ODIR)/*
