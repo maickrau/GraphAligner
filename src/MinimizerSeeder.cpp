74aa8bd4ab8896241ef8d6afd3e61b89e34247f6
Mikko Rautiainen <m_rautiainen@hotmail.com>
2019-10-02 17:45:30 +0200

    More parallel index construction

--- src/MinimizerSeeder.cpp
+++ src/MinimizerSeeder.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 #include <cmath>
+#include <concurrentqueue.h>
 #include "CommonUtils.h"
 #include "MinimizerSeeder.h"
 
@@ -178,9 +179,7 @@ start:
 
 MinimizerSeeder::MinimizerSeeder(const AlignmentGraph& graph, size_t minimizerLength, size_t windowSize, size_t numThreads) :
 graph(graph),
-locator(),
-starts(),
-positions(),
+buckets(),
 minimizerLength(minimizerLength),
 windowSize(windowSize),
 maxCount(0)
@@ -234,22 +233,28 @@ void MinimizerSeeder::initMinimizers(size_t numThreads)
 {
 	size_t positionSize = log2(graph.nodeIDs.size()) + 1;
 	assert(minimizerLength * 2 + positionSize + 6 < 128);
-	positions.width(positionSize+6);
 	auto nodeIter = graph.nodeLookup.begin();
 	std::mutex nodeMutex;
 	std::vector<std::thread> threads;
 	std::vector<sdsl::int_vector<0>> resultPerThread;
+	std::vector<moodycamel::ConcurrentQueue<unsigned __int128>> positionDistributor;
 	resultPerThread.resize(numThreads);
+	positionDistributor.resize(numThreads);
+	buckets.resize(numThreads);
+	std::atomic<size_t> threadsDone;
+	threadsDone = 0;
 	for (size_t i = 0; i < numThreads; i++)
 	{
-		resultPerThread[i].width(2 * minimizerLength + positionSize + 1);
+		resultPerThread[i].width(2 * minimizerLength + positionSize + 6);
+		buckets[i].positions.width(positionSize + 6);
 	}
 
-	for (size_t i = 0; i < numThreads; i++)
+	for (size_t thread = 0; thread < numThreads; thread++)
 	{
-		threads.emplace_back([this, &resultPerThread, i, &nodeMutex, &nodeIter, positionSize](){
+		threads.emplace_back([this, &positionDistributor, &threadsDone, &resultPerThread, thread, numThreads, &nodeMutex, &nodeIter, positionSize](){
 			size_t vecPos = 0;
-			resultPerThread[i].resize(10);
+			resultPerThread[thread].resize(10);
+			unsigned __int128 readThis;
 			while (true)
 			{
 				auto iter = graph.nodeLookup.end();
@@ -263,98 +268,128 @@ void MinimizerSeeder::initMinimizers(size_t numThreads)
 				int firstNode = iter->second[0];
 				std::string sequence;
 				sequence.resize(graph.originalNodeSize.at(nodeId));
-				for (size_t i = 0; i < sequence.size(); i++)
+				for (size_t pos = 0; pos < sequence.size(); pos++)
 				{
-					size_t nodeidHere = graph.GetUnitigNode(nodeId, i);
-					sequence[i] = graph.NodeSequences(nodeidHere, i - graph.nodeOffset[nodeidHere]);
+					size_t nodeidHere = graph.GetUnitigNode(nodeId, pos);
+					sequence[pos] = graph.NodeSequences(nodeidHere, pos - graph.nodeOffset[nodeidHere]);
 				}
-				iterateMinimizers(sequence, minimizerLength, windowSize, [this, &resultPerThread, &vecPos, positionSize, i, nodeId](size_t pos, size_t kmer)
+				iterateMinimizers(sequence, minimizerLength, windowSize, [this, &positionDistributor, &resultPerThread, &vecPos, positionSize, thread, nodeId](size_t pos, size_t kmer)
 				{
 					size_t splitNode = graph.GetUnitigNode(nodeId, pos);
 					size_t remainingOffset = pos - graph.nodeOffset[splitNode];
 					assert(remainingOffset < 64);
+					unsigned __int128 readThis;
+					while (positionDistributor[thread].try_dequeue(readThis))
+					{
+						size_t kmer = readThis >> (positionSize + 6);
+						assert(getBucket(kmer) == thread);
+						if (vecPos == resultPerThread[thread].size()) resultPerThread[thread].resize(resultPerThread[thread].size() * 2);
+						resultPerThread[thread][vecPos] = readThis;
+						assert((unsigned __int128)resultPerThread[thread][vecPos] == readThis);
+						assert(getBucket((unsigned __int128)resultPerThread[thread][vecPos] >> (positionSize + 6)) == thread);
+						vecPos += 1;
+					}
 					unsigned __int128 storeThis = kmer;
+					size_t bucket = getBucket(kmer);
 					storeThis <<= positionSize;
 					storeThis += splitNode;
 					storeThis <<= 6;
 					storeThis += remainingOffset;
-					if (vecPos == resultPerThread[i].size()) resultPerThread[i].resize(resultPerThread[i].size() * 2);
-					resultPerThread[i][vecPos] = storeThis;
-					vecPos += 1;
+					positionDistributor[bucket].enqueue(storeThis);
 				});
 			}
-			std::sort(resultPerThread[i].begin(), resultPerThread[i].end(), [positionSize](unsigned __int128 left, unsigned __int128 right) { return left < right; });
+			threadsDone += 1;
+			while (threadsDone < numThreads)
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				while (positionDistributor[thread].try_dequeue(readThis))
+				{
+					size_t kmer = readThis >> (positionSize + 6);
+					assert(getBucket(kmer) == thread);
+					if (vecPos == resultPerThread[thread].size()) resultPerThread[thread].resize(resultPerThread[thread].size() * 2);
+					resultPerThread[thread][vecPos] = readThis;
+					assert((unsigned __int128)resultPerThread[thread][vecPos] == readThis);
+					assert(getBucket((unsigned __int128)resultPerThread[thread][vecPos] >> (positionSize + 6)) == thread);
+					vecPos += 1;
+				}
+			}
+			while (positionDistributor[thread].try_dequeue(readThis))
+			{
+				size_t kmer = readThis >> (positionSize + 6);
+				assert(getBucket(kmer) == thread);
+				if (vecPos == resultPerThread[thread].size()) resultPerThread[thread].resize(resultPerThread[thread].size() * 2);
+				resultPerThread[thread][vecPos] = readThis;
+				assert((unsigned __int128)resultPerThread[thread][vecPos] == readThis);
+				assert(getBucket((unsigned __int128)resultPerThread[thread][vecPos] >> (positionSize + 6)) == thread);
+				vecPos += 1;
+			}
+			resultPerThread[thread].resize(vecPos);
+			std::sort(resultPerThread[thread].begin(), resultPerThread[thread].end(), [positionSize](unsigned __int128 left, unsigned __int128 right) { return left < right; });
+			size_t current = std::numeric_limits<size_t>::max();
+			std::vector<size_t> locatorKeys;
+			for (unsigned __int128 value : resultPerThread[thread])
+			{
+				size_t kmer = value >> (positionSize + 6);
+				assert(getBucket(kmer) == thread);
+				if (kmer == current)
+				{
+					continue;
+				}
+				current = kmer;
+				locatorKeys.push_back(current);
+			}
+			buckets[thread].locator = new boomphf::mphf<size_t,KmerBucket::hasher_t>(locatorKeys.size(), locatorKeys, 1, 2, true, false);
+			{
+				decltype(locatorKeys) tmp;
+				std::swap(tmp, locatorKeys);
+			}
+			sdsl::int_vector<0> counts;
+			counts.width(log2(resultPerThread[thread].size()) + 1);
+			counts.resize(buckets[thread].locator->nbKeys());
+			buckets[thread].kmerCheck.width(minimizerLength * 2);
+			buckets[thread].kmerCheck.resize(buckets[thread].locator->nbKeys());
+			sdsl::util::set_to_value(counts, 0);
+			for (unsigned __int128 value : resultPerThread[thread])
+			{
+				size_t kmer = value >> (positionSize + 6);
+				size_t index = buckets[thread].locator->lookup(kmer);
+				counts[index] += 1;
+				buckets[thread].kmerCheck[index] = kmer;
+			}
+			buckets[thread].starts.resize(resultPerThread[thread].size());
+			sdsl::util::set_to_value(buckets[thread].starts, 0);
+			buckets[thread].starts[0] = 1;
+			buckets[thread].starts[counts[0]] = 1;
+			for (size_t i = 1; i < counts.size()-1; i++)
+			{
+				counts[i] += counts[i-1];
+				buckets[thread].starts[counts[i]] = 1;
+			}
+			if (counts.size() >= 2) counts[counts.size()-1] += counts[counts.size()-2];
+			assert(counts[counts.size()-1] == resultPerThread[thread].size());
+			buckets[thread].positions.resize(resultPerThread[thread].size());
+			for (auto value : resultPerThread[thread])
+			{
+				size_t kmer = value >> (positionSize + 6);
+				size_t index = buckets[thread].locator->lookup(kmer);
+				assert(counts[index] > 0);
+				counts[index] -= 1;
+				size_t pos = counts[index];
+				unsigned __int128 insert = value & ~(-1 << (positionSize + 6));
+				assert(insert < (unsigned __int128)(1 << (positionSize + 6)));
+				buckets[thread].positions[pos] = insert;
+			};
+
+			sdsl::util::init_support(buckets[thread].startSelector, &buckets[thread].starts);
+			assert(buckets[thread].positions.size() == resultPerThread[thread].size());
 		});
 	}
-
-	size_t totalSize = 0;
 
 	for (size_t i = 0; i < threads.size(); i++)
 	{
 		threads[i].join();
-		totalSize += resultPerThread[i].size();
 	}
 	threads.clear();
-
-	size_t current = std::numeric_limits<size_t>::max();
-	std::vector<size_t> locatorKeys;
-	iteratePerThreads(resultPerThread, positionSize, [&current, positionSize, &locatorKeys](unsigned __int128 value)
-	{
-		size_t kmer = value >> (positionSize + 6);
-		if (kmer == current)
-		{
-			return;
-		}
-		current = kmer;
-		locatorKeys.push_back(current);
-	});
-	locator.build(numThreads, locatorKeys);
-	{
-		decltype(locatorKeys) tmp;
-		std::swap(tmp, locatorKeys);
-	}
-	sdsl::int_vector<0> counts;
-	counts.width(log2(totalSize) + 1);
-	counts.resize(locator.size());
-	kmerCheck.width(minimizerLength * 2);
-	kmerCheck.resize(locator.size());
-	sdsl::util::set_to_value(counts, 0);
-	iteratePerThreads(resultPerThread, positionSize, [this, &counts, positionSize](unsigned __int128 value)
-	{
-		size_t kmer = value >> (positionSize + 6);
-		size_t index = locator.lookup(kmer);
-		counts[index] += 1;
-		kmerCheck[index] = kmer;
-	});
-	starts.resize(totalSize);
-	sdsl::util::set_to_value(starts, 0);
-	starts[0] = 1;
-	starts[counts[0]] = 1;
-	for (size_t i = 1; i < counts.size()-1; i++)
-	{
-		counts[i] += counts[i-1];
-		starts[counts[i]] = 1;
-	}
-	if (counts.size() >= 2) counts[counts.size()-1] += counts[counts.size()-2];
-	assert(counts[counts.size()-1] == totalSize);
-	positions.resize(totalSize);
-	iteratePerThreads(resultPerThread, positionSize, [this, &counts, positionSize](unsigned __int128 value)
-	{
-		size_t kmer = value >> (positionSize + 6);
-		size_t index = locator.lookup(kmer);
-		assert(counts[index] > 0);
-		counts[index] -= 1;
-		size_t pos = counts[index];
-		unsigned __int128 insert = value & ~(-1 << (positionSize + 6));
-		assert(insert < (unsigned __int128)(1 << (positionSize + 6)));
-		positions[pos] = insert;
-	});
-
-	sdsl::util::init_support(startSelector, &starts);
-	assert(positions.size() == totalSize);
-	std::cerr << "locator: " << locator.size() << std::endl;
-	std::cerr << "starts: " << starts.size() << " " << starts.capacity() << std::endl;
-	std::cerr << "positions: " << positions.size() << " " << positions.capacity() << std::endl;
 }
 
 std::vector<SeedHit> MinimizerSeeder::getSeeds(const std::string& sequence, size_t maxCount, size_t chunkSize) const
@@ -365,12 +400,15 @@ std::vector<SeedHit> MinimizerSeeder::getSeeds(const std::string& sequence, size
 	matchIndices.resize(numChunks);
 	iterateMinimizers(sequence, minimizerLength, windowSize, [this, &matchIndices, bpPerChunk](size_t pos, size_t kmer)
 	{
-		auto index = locator.lookup(kmer);
+		volatile size_t bucket = getBucket(kmer);
+		assert(bucket < buckets.size());
+		volatile size_t index = buckets[bucket].locator->lookup(kmer);
 		if (index == ULLONG_MAX) return;
-		if (kmerCheck[index] != kmer) return;
+		assert(index < buckets[bucket].kmerCheck.size());
+		if (buckets[bucket].kmerCheck[(size_t)index] != kmer) return;
 		size_t chunk = pos / bpPerChunk;
 		assert(chunk < matchIndices.size());
-		matchIndices[chunk].emplace_back(pos, kmer, getStart(index+1) - getStart(index));
+		matchIndices[chunk].emplace_back(pos, kmer, getStart(bucket, index+1) - getStart(bucket, index));
 	});
 	//prefer less common minimizers
 	for (size_t i = 0; i < numChunks; i++)
@@ -386,12 +424,13 @@ std::vector<SeedHit> MinimizerSeeder::getSeeds(const std::string& sequence, size
 		size_t seedsHere = 0;
 		for (auto match : matchIndices[i])
 		{
-			auto found = locator.lookup(std::get<1>(match));
-			size_t end = getStart(found+1);
-			for (size_t i = getStart(found); i < end; i++)
+			size_t bucket = getBucket(std::get<1>(match));
+			auto found = buckets[bucket].locator->lookup(std::get<1>(match));
+			size_t end = getStart(bucket, found+1);
+			for (size_t i = getStart(bucket, found); i < end; i++)
 			{
 				if (seedsHere >= maxCount) break;
-				volatile size_t mergepos = positions[i];
+				size_t mergepos = buckets[bucket].positions[i];
 				result.push_back(matchToSeedHit((size_t)(mergepos / 64), mergepos % 64, std::get<0>(match), std::get<2>(match)));
 				seedsHere += 1;
 			}
@@ -413,14 +452,31 @@ SeedHit MinimizerSeeder::matchToSeedHit(int nodeId, size_t nodeOffset, size_t se
 void MinimizerSeeder::initMaxCount()
 {
 	maxCount = 0;
-	for (size_t i = 0; i < locator.size()-1; i++)
+	for (size_t bucket = 0; bucket < buckets.size(); bucket++)
 	{
-		maxCount = std::max(maxCount, getStart(i+1) - getStart(i));
+		for (size_t i = 0; i < buckets[bucket].locator->nbKeys()-1; i++)
+		{
+			maxCount = std::max(maxCount, getStart(bucket, i+1) - getStart(bucket, i));
+		}
 	}
 	maxCount += 1;
 }
 
-size_t MinimizerSeeder::getStart(size_t index) const
+size_t MinimizerSeeder::getStart(size_t bucket, size_t index) const
 {
-	return startSelector(index+1);
+	return buckets[bucket].startSelector(index+1);
+}
+
+size_t MinimizerSeeder::getBucket(size_t hash) const
+{
+	return hash % buckets.size();
+}
+
+MinimizerSeeder::KmerBucket::KmerBucket() :
+	locator(nullptr)
+{}
+
+MinimizerSeeder::KmerBucket::~KmerBucket()
+{
+	if (locator != nullptr) delete locator;
 }
--- src/MinimizerSeeder.h
+++ src/MinimizerSeeder.h
@@ -9,23 +9,37 @@
 #include <ParallelBB.h>
 #include "AlignmentGraph.h"
 #include "GraphAlignerWrapper.h"
+#include "BooPHF.h"
 
 class MinimizerSeeder
 {
+	struct KmerBucket
+	{
+		KmerBucket();
+		KmerBucket(const KmerBucket& other) = delete;
+		KmerBucket(KmerBucket&& other) = default;
+		~KmerBucket();
+		KmerBucket& operator=(const KmerBucket& other) = delete;
+		KmerBucket& operator=(KmerBucket&& other) = default;
+		typedef boomphf::SingleHashFunctor<size_t> hasher_t;
+		typedef boomphf::mphf<size_t, hasher_t> boophf_t;
+		boophf_t* locator;
+		sdsl::int_vector<0> kmerCheck;
+		sdsl::bit_vector starts;
+		sdsl::bit_vector::select_1_type startSelector;
+		sdsl::int_vector<0> positions;
+	};
 public:
 	MinimizerSeeder(const AlignmentGraph& graph, size_t minimizerLength, size_t windowSize, size_t numThreads);
 	std::vector<SeedHit> getSeeds(const std::string& sequence, size_t maxCount, size_t chunkSize) const;
 private:
-	size_t getStart(size_t index) const;
-	void initMinimizers(size_t numThreads);
+	size_t getStart(size_t bucket, size_t index) const;
+	size_t getBucket(size_t hash) const;
 	SeedHit matchToSeedHit(int nodeId, size_t nodeOffset, size_t seqPos, int count) const;
+	void initMinimizers(size_t numThreads);
 	void initMaxCount();
 	const AlignmentGraph& graph;
-	ParallelBB<size_t> locator;
-	sdsl::int_vector<0> kmerCheck;
-	sdsl::bit_vector starts;
-	sdsl::bit_vector::select_1_type startSelector;
-	sdsl::int_vector<0> positions;
+	std::vector<KmerBucket> buckets;
 	size_t minimizerLength;
 	size_t windowSize;
 	size_t maxCount;
